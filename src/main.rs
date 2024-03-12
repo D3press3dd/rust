c9af9e5b7a574f9556b04a99f3d4264aab86d9a6
d3press3d <anthonyrosman20@gmail.com>
2024-03-12 07:08:55 -0400

    chore: add rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read the variable");
+
+    println!("you guessed {guess}");
 }
